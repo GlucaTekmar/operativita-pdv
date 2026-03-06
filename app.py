f09983f0dcea46ccee581df7d576bb78072ecebf
GlucaTekmar <gianluca.colantoni@tekmarpromo.it>
2026-03-07 00:49:28 +0100

    Update app.py

--- app.py
+++ app.py
@@ -398,9 +398,8 @@ def admin():
         if st.button("SALVA MESSAGGIO"):
             if not os.path.exists(MSG_FILE):
 
-                pd.DataFrame(columns=["msg","inizio","fine","pdv_ids","file"])
-                df = load_csv(MSG_FILE, 
-                ["msg", "inizio", "fine", "pdv_ids", "file"])
+                pd.DataFrame(columns=["msg","inizio","fine","pdv_ids","file"]).to_csv(MSG_FILE,index=False)
+                df = load_csv(MSG_FILE,["msg", "inizio", "fine", "pdv_ids", "file"])
 
             filename = ""
             
@@ -409,7 +408,7 @@ def admin():
                 filepath = f"media/{filename}"
                 os.makedirs("media", exist_ok=True)
                 with open(fiilepath, "wb") as f:
-                f.write(uploaded.getbuffer())
+                    f.write(uploaded.getbuffer())
                 filename = filepath
 
          new = pd.DataFrame([[
@@ -728,6 +727,7 @@ if st.query_params.get("admin") == "1":
     admin()
 else:
     dipendenti()
+
 
 
 
